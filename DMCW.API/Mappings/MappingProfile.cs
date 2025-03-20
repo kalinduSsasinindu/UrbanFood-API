a17d8528ab81c6bec93d351cc85413a004b95348
kalinduSsasinindu <kalindusasinindu153@gmail.com>
2025-03-21 00:55:09 +0530

    added mappingProfile

---
+++ DMCW.API/Mappings/MappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using DMCW.Repository.Data.Entities.User;
+using DMCW.ServiceInterface.Dtos.User.KlzTEch.Service.Interface.Dto;
+
+namespace DMCW.API.Mappings
+{
+    using AutoMapper;
+    using DMCW.Repository.Data.Entities.User;
+
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserServiceDto>();
+            CreateMap<Address, AddressDto>();
+            CreateMap<GeoCoordinates, GeoCoordinatesDto>();
+            CreateMap<BusinessHours, BusinessHoursDto>();
+
+            // Reverse mappings if needed
+            CreateMap<UserServiceDto, User>();
+            CreateMap<AddressDto, Address>();
+            CreateMap<GeoCoordinatesDto, GeoCoordinates>();
+            CreateMap<BusinessHoursDto, BusinessHours>();
+        }
+    }
+}
